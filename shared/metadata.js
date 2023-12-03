d1578260e56bc7e31cdb9f3ba5b9b4d5dc982d15
Aarón <aaron@MacBook-Pro-de-Aaron.local>
2023-12-03 19:53:59 +0000

    metadata file

---
+++ shared/metadata.js
@@ -0,0 +1,40 @@
+export const METADATA = {
+  metadataBase: new URL('https://devbug.es'),
+  title: 'Debvug - Insert Slogan Here :D',
+  description: 'Debvug - I forgot insert description here :P',
+  keywords: 'dev, bug, developers',
+  openGraph: {
+    title: 'Debvug - Insert Slogan Here :D',
+    description: 'Debvug - I forgot insert description here :P',
+    images: [{ url: '/images/opengraph.jpg' }],
+  },
+  twitter: {
+    title: 'Debvug - Insert Slogan Here :D',
+    description: 'Debvug - I forgot insert description here :P',
+    images: [{ url: '/images/opengraph.jpg' }],
+  },
+  icons: {
+    icon: [
+      {
+        type: 'image/png',
+        sizes: '16x16',
+        url: '/images/favicon/16x16.png',
+      },
+      {
+        type: 'image/png',
+        sizes: '32x32',
+        url: '/images/favicon/32x32.png',
+      },
+      {
+        type: 'image/png',
+        sizes: '64x64',
+        url: '/images/favicon/64x64.png',
+      },
+      {
+        type: 'image/png',
+        sizes: '128x128',
+        url: '/images/favicon/128x128.png',
+      },
+    ],
+  },
+};
